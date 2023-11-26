5bc4899f8c955e3cee6f2068f1e6d21696f88f39
Алексей Назаров <AlSNazarov@1cbit.ru>
2023-11-26 22:00:39 +0500

    Update logger

---
+++ src/Debug/Logger.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ==================================================
+ * Developer: Alexey Nazarov
+ * E-mail: jc1988x@gmail.com
+ * Copyright (c) 2019 - 2023
+ * ==================================================
+ * bit-umc-php-sdk - Logger.php
+ * 26.11.2023 21:44
+ * ==================================================
+ */
+
+
+namespace ANZ\BitUmc\SDK\Debug;
+
+class Logger
+{
+
+}
